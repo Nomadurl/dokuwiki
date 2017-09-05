f253706f6316f279ee4fe4a476f880aa1ceea902
i.kapustin <ilja.uralsk@mail.ru>
2017-09-05 16:08:37 +0400

    first commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+root:$1$tEPaLuik$b2ngYHjaNvfxUnTqJTumG/:i.kapustin:ilja.uralsk@mail.ru:admin,user
